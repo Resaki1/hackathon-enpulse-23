4b4cc391e51eab5a39f38250c467a7b9d77a8251
Resaki1 <christian.reski@t-online.de>
2023-04-21 15:20:58 +0200

    added logout

--- src/App.tsx
+++ src/App.tsx
@@ -9,6 +9,7 @@ function App() {
   const [text, setText] = useState("");
   const [result, setResult] = useState<string>("");
   const [loading, setLoading] = useState(false);
+  const [login, setLogin] = useState(true);
 
   const search = async (text: string) => {
     setLoading(true);
@@ -23,45 +24,55 @@ function App() {
 
   return (
     <div className="App">
+      <div className="header">
+        <a onClick={() => setLogin(!login)}>{login ? "logout" : "login"}</a>
+      </div>
       <div>
         <h1>
           <img src={TwinCity} alt="TwinCity Logo" />
         </h1>
         <h2>Stadt Karlsruhe</h2>
       </div>
-      <div className="content">
-        <h3>Wonach suchst du?</h3>
-        <p>
-          Frage zum Beispiel nach der Tiefe von Leitungen in einer bestimmten
-          Straße.
-        </p>
-        <input
-          type="text"
-          value={text}
-          placeholder="Was ist die maximale Grabtiefe in der Moltkestraße?"
-          onChange={(e) => setText(e.target.value)}
-          onKeyDown={(e) => {
-            if (e.key === "Enter") {
-              search(text);
-            }
-          }}
-        ></input>
-        {loading && <div className="dot-elastic"></div>}
-        {result && (
-          <>
-            <p>{result}</p>
-            <p>
-              Lade dir{" "}
-              <a href="./data.html" target="_blank">
-                hier
-              </a>{" "}
-              die Daten runter.
-            </p>
-          </>
-        )}
-      </div>
+      {login && (
+        <div className="content">
+          <h3>Wonach suchst du?</h3>
+          <p>
+            Frage zum Beispiel nach der Tiefe von Leitungen in einer bestimmten
+            Straße.
+          </p>
+          <input
+            type="text"
+            value={text}
+            placeholder="Was ist die maximale Grabtiefe in der Moltkestraße?"
+            onChange={(e) => setText(e.target.value)}
+            onKeyDown={(e) => {
+              if (e.key === "Enter") {
+                search(text);
+              }
+            }}
+          ></input>
+          {loading && <div className="dot-elastic"></div>}
+          {result && (
+            <>
+              <p>{result}</p>
+              <p>
+                Lade dir{" "}
+                <a href="./data.html" target="_blank">
+                  hier
+                </a>{" "}
+                die Daten runter.
+              </p>
+            </>
+          )}
+        </div>
+      )}
       <div className="footer">
-        TwinCity | Ein Produkt von{" "}
+        TwinCity Karlsruhe
+        <img
+          height="32"
+          src="https://www.cityofmediaarts.de/wp-content/uploads/2021/05/logo_karlsruhe.png"
+        />
+        | Ein Produkt von{" "}
         <a href="https://enpulse.io/" target="_blank">
           enpulse
         </a>
